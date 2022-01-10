ca860580c5483f8a002fa3b7de65d640de19c9b1
Juan Carlos <jchernandez827@gmail.com>
2022-01-10 15:31:48 -0500

    Add queries to select data from animals table

--- README.md
+++ README.md
@@ -12,6 +12,7 @@ This repository includes files with plain SQL that can be used to recreate a dat
 - Use [data.sql](./data.sql) to populate tables with sample data.
 - Check [queries.sql](./queries.sql) for examples of queries that can be run on a newly created database. **Important note: this file might include queries that make changes in the database (e.g., remove records). Use them responsibly!**
 
+![screenshot](https://user-images.githubusercontent.com/44485810/148834789-ce722fda-27ee-46d7-a232-a3ac17a47d71.png)
 
 ## Authors
 
--- queries.sql
+++ queries.sql
@@ -1,3 +1,25 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+-- Animals that name ends with mon
+SELECT * FROM animals WHERE name LIKE '%mon%';
+
+-- Animals born between 2016 and 2019
+SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
+
+-- Animals neutered and with less than 3 escape attempts
+SELECT name FROM animals WHERE neutered = true AND escape_attempts < 3;
+
+-- Date of birth animals named Pikachu or agumon
+SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
+
+-- Name and escape_attempts from animals that weigh less than 10.5kg
+SELECT name, escape_attempts FROM animals WHERE weight_kg < 10.5;
+
+-- Neutered animals
+SELECT * FROM animals WHERE neutered = true;
+
+-- Animals not named gabumon
+SELECT * FROM animals WHERE name != 'Gabumon';
+
+-- Animals with weight more or equal to 10.4 and less or equal to 17.3
+SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
