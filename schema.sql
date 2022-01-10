ce3bc08ff9540faf179fccfe1a447d83ca7c25af
Juan Carlos <jchernandez827@gmail.com>
2022-01-10 14:40:26 -0500

    Create vet_clinic db, create animals table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,15 @@
 /* Database schema to keep the structure of entire database. */
 
+-- Create vet_clinic DB
+CREATE DATABASE vet_clinic;
+
+-- Create animals table
 CREATE TABLE animals (
-    name varchar(100)
+  id INT, 
+  name VARCHAR(50), 
+  date_of_birth DATE, 
+  escape_attempts INT, 
+  neutered BOOLEAN, 
+  weight_kg DECIMAL, 
+  PRIMARY KEY(id)
 );
