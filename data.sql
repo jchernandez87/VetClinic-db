45a7148a3c3c6f6d48175fb62770b0d093590f9b
Juan Carlos <jchernandez827@gmail.com>
2022-01-12 12:50:00 -0500

    update data.sql

--- data.sql
+++ data.sql
@@ -9,3 +9,31 @@ INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_k
 INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(8, 'Angemon', '2005-06-12', 1, true, -45);
 INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(9, 'Boarmon', '2005-06-07', 7, true, 20.4);
 INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(10, 'Blossom', '1998-10-13', 3, true, 17);
+
+-- Insert new record into owners 
+-- In here use the syntax to insert multiple rows at once
+INSERT INTO owners(full_name, age) VALUES('Sam Smith', 34), ('Jennifer Orwell', 19), ('Bob', 45), ('Melody Pond', 77), ('Dean Winchester', 14), ('Jodie Whittaker', 38);
+
+-- Insert pokemon and digimon record into species table
+INSERT INTO species(name) VALUES ('Pokemon'), ('Digimon');
+
+-- Update animals table, so species_id will be 2 if animal name ends with mon
+UPDATE animals SET species_id = 2 WHERE name LIKE '%mon';
+
+-- Update animals table, so species_id will be 1 where species_id = NULL
+UPDATE animals SET species_id = 1 WHERE species_id IS NULL;
+
+-- Sam smith(id = 1) owns agumon
+UPDATE animals SET owner_id = 1 WHERE name = 'Agumon';
+
+-- Jennifer orwell(id = 2) owns gabumon and pikachu
+UPDATE animals SET owner_id = 2 WHERE name = 'Gabumon' OR name = 'Pikachu';
+
+-- Bob(id = 3) owns devimon and plantmon
+UPDATE animals SET owner_id = 3 WHERE name = 'Devimon' OR name = 'Plantmon';
+
+-- Melody pond(id = 4) owns charmander, squirtle and blossom
+UPDATE animals SET owner_id = 4 WHERE name = 'Charmander' OR name = 'Squirtle' OR name = 'Blossom';
+
+-- Dean winchester(id = 5) owns angemon and boarmon
+UPDATE animals SET owner_id = 5 WHERE name = 'Angemon' OR name = 'Boarmon';
